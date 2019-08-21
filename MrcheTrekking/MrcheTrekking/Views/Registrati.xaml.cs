62a69cd4905db6ca822a3731c30359831c549e57
Geni Mucalla <mucalla.geni@gmail.com>
2019-08-21 12:23:34 +0200

    database

--- MrcheTrekking/MrcheTrekking/Views/Registrati.xaml.cs
+++ MrcheTrekking/MrcheTrekking/Views/Registrati.xaml.cs
@@ -16,5 +16,43 @@ namespace MrcheTrekking.Views
 		{
 			InitializeComponent ();
 		}
+         //async
+        protected async void Reg(object sender, EventArgs args)
+        {
+            
+            string n = nome.Text;
+            string c = cognome.Text;
+            string e = email.Text;
+            string u = username.Text;
+            string t = telefono.Text;
+            string p = password.Text;
+            string c = datadinascita.Text;
+
+            var uri = "http://marchetrekking.altervista.org/aggiungi_utente.php";
+
+            //body della post request
+            var content = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string,string> ("user", u),
+                new KeyValuePair<string,string> ("pass", p),
+                new KeyValuePair<string,string> ("mail", e),
+                new KeyValuePair<string,string> ("n", n),
+                new KeyValuePair<string,string> ("c", c),
+                new KeyValuePair<string,string> ("telef", t),
+            });
+
+            //inoltro richiesta al server
+            HttpClient client = new HttpClient();
+            var response = await client.PostAsync(uri, content);
+            var risp = await response.Content.ReadAsStringAsync();  //risposta del server
+            String s = risp.Trim();
+            if(s.Equals("1"))
+            {
+                MrcheTrekking.Utility.Settings.User = u;    //uso del pacchetto nuget Xamarin Settings per abilitare una "sessione"
+                Navigation.InsertPageBefore(new Home(), this);
+                await Navigation.PopAsync();    //rimuove la precedente pagina dallo stack
+            }
+
+         }
 	}
 }
