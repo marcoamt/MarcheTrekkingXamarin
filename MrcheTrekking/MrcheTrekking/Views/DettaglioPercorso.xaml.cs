7e30a51b4944fbb6f75368c4a78a943572e98b1e
marcomendoza <48435302+marcoamt@users.noreply.github.com>
2019-08-23 11:43:04 +0200

    Add files via upload

---
+++ MrcheTrekking/MrcheTrekking/Views/DettaglioPercorso.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using MrcheTrekking.Models;
+using Xamarin.Forms;
+
+namespace MrcheTrekking.Views
+{
+    public partial class DettaglioPercorso : ContentPage
+    {
+        public DettaglioPercorso(Object a)
+        {
+            InitializeComponent();
+
+            PercorsiModel p = (MrcheTrekking.Models.PercorsiModel)a;
+
+            string nome = p.Nome;
+            string desc = p.Descrizione;
+            double lunghezza = p.Lunghezza;
+            int livello = p.Livello;
+            string durata = p.Durata;
+            string img = p.Immagine;
+            string map = p.Mappa;
+
+            descrizione.Text = "Nome: " + nome +
+                                "\nDescrizione: " + desc +
+                                "\nLunghezza: " + lunghezza +
+                                "\nLivello: " + livello +
+                                "\nDurata: " + durata ;
+
+
+        }
+    }
+}
